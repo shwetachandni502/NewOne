d6d32a25b7c207d388e4d4bef4ad60b95ccf08fe
Shweta Verma <shweta.verma@ad.infosys.com>
2022-03-02 15:03:58 +0530

    second commit

--- Routes/authRoute.js
+++ Routes/authRoute.js
@@ -4,7 +4,7 @@ const auth = require('../Controller/authController');
 
  routing.post('/signup', auth.signup);
  routing.post('/login', auth.login);
- routing.post('/data', auth.data);
+ routing.get('/data', auth.data);
  routing.all('*', auth.inValid);
 
 module.exports = routing;
