776ec356998aa7511aa0f96af5470eddc0f6aadf
shweta <uadtech130792@gmail.com>
2022-04-05 14:23:56 +0530

    OTP

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use('/user', userRoute);
 app.use('/merchant', merchantRoute);
 app.use('/provider', providerRoute);
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 6000;
 app.listen(port, () =>{
     console.log(`App running on port ${port} ...`);
 });
