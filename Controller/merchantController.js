f73d1a8de9e53e075428f9edcdc346f46153aa2f
shweta <uadtech130792@gmail.com>
2022-05-08 14:15:59 +0530

    merchant

--- Controller/merchantController.js
+++ Controller/merchantController.js
@@ -29,7 +29,7 @@ exports.addParking = async (req, res) => {
     try {
         console.log("log of ADD PARKING_---")
         const { parkingName, price, address, name, phoneNumber, about } = req.body;
-        const { parkingImage } = req.files;
+        // const { parkingImage } = req.files;
         const check_exist = await Auth.findById(req.data.id);
         if (!check_exist) return res.status(404).json({ error: 'User not found' })
 
@@ -43,7 +43,8 @@ exports.addParking = async (req, res) => {
                 phoneNumber
             },
             about,
-            parkingImage: keys.apiURL + parkingImage[0].filename || keys.apiURL + "default.png"
+            parkingImage: keys.apiURL + "default.png"
+            // parkingImage: keys.apiURL + parkingImage[0].filename || keys.apiURL + "default.png"
         });
         const save = await new_parking.save();
         return res.status(200).json({
